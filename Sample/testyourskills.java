9b74764e10dfe087a8e1d30014d003d2ae4aaa4f
Nagamuthulakshmi <59141128+Nagamuthulakshmi@users.noreply.github.com>
2023-04-19 17:07:00 +0530

    Add files via upload

---
+++ Sample/testyourskills.java
@@ -0,0 +1,10 @@
+public class testyourskills {
+    public static void main(String[] args) {
+        int var1=10,var2=5;
+        System.out.println(var1+var2);
+        System.out.println(var1-var2);
+        System.out.println(var1/var2);
+        System.out.println(var1*var2);
+        System.out.println(var1%var2);
+    }
+}
